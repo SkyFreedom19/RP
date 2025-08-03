e42b55baffb6896db1cb1ceef336ec189b4463b2
SkyFreedom19 <145896234+SkyFreedom19@users.noreply.github.com>
2025-08-03 19:57:27 +0700

    Add files via upload

---
+++ db_masjid.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 03, 2025 at 02:57 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_masjid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kas_pemasukan`
+--
+-- Error reading structure for table db_masjid.kas_pemasukan: #1932 - Table &#039;db_masjid.kas_pemasukan&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_masjid.kas_pemasukan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_masjid`.`kas_pemasukan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kas_pengeluaran`
+--
+-- Error reading structure for table db_masjid.kas_pengeluaran: #1932 - Table &#039;db_masjid.kas_pengeluaran&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_masjid.kas_pengeluaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_masjid`.`kas_pengeluaran`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
